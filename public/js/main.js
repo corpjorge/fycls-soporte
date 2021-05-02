6b9c13d4d19a474c0af5a82f0ef2a9c743815b20
Jorge Eduardo <corpjorge@hotmail.com>
2021-05-02 18:54:25 -0500

    Ajustes al menu

--- public/js/main.js
+++ public/js/main.js
@@ -16955,7 +16955,13 @@ var _hoisted_2 = {
   "class": "container-fluid py-1 px-3"
 };
 
-var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<nav aria-label=\"breadcrumb\" data-v-8956fabe><ol class=\"breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5\" data-v-8956fabe><li class=\"breadcrumb-item text-sm\" data-v-8956fabe><a class=\"opacity-5 text-dark\" href=\"javascript:;\" data-v-8956fabe>Pages</a></li><li class=\"breadcrumb-item text-sm text-dark active\" aria-current=\"page\" data-v-8956fabe>Tables</li></ol><h6 class=\"font-weight-bolder mb-0\" data-v-8956fabe>Tables</h6></nav>", 1);
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("nav", {
+  "aria-label": "breadcrumb"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("h6", {
+  "class": "font-weight-bolder mb-0"
+}, "Tables")], -1
+/* HOISTED */
+);
 
 var _hoisted_4 = {
   "class": "navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4",
@@ -17194,11 +17200,11 @@ var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("svg", {
   width: "12px",
   height: "12px",
-  viewBox: "0 0 42 42",
+  viewBox: "0 0 45 40",
   version: "1.1",
   xmlns: "http://www.w3.org/2000/svg",
   "xmlns:xlink": "http://www.w3.org/1999/xlink"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("title", null, "office"), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("g", {
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("title", null, "shop "), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("g", {
   id: "Basic-Elements",
   stroke: "none",
   "stroke-width": "1",
@@ -17206,24 +17212,24 @@ var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("
   "fill-rule": "evenodd"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("g", {
   id: "Rounded-Icons",
-  transform: "translate(-1869.000000, -293.000000)",
+  transform: "translate(-1716.000000, -439.000000)",
   fill: "#FFFFFF",
   "fill-rule": "nonzero"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("g", {
   id: "Icons-with-opacity",
   transform: "translate(1716.000000, 291.000000)"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("g", {
-  id: "office",
-  transform: "translate(153.000000, 2.000000)"
+  id: "shop-",
+  transform: "translate(0.000000, 148.000000)"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("path", {
   "class": "color-background",
-  d: "M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z",
+  d: "M46.7199583,10.7414583 L40.8449583,0.949791667 C40.4909749,0.360605034 39.8540131,0 39.1666667,0 L7.83333333,0 C7.1459869,0 6.50902508,0.360605034 6.15504167,0.949791667 L0.280041667,10.7414583 C0.0969176761,11.0460037 -1.23209662e-05,11.3946378 -1.23209662e-05,11.75 C-0.00758042603,16.0663731 3.48367543,19.5725301 7.80004167,19.5833333 L7.81570833,19.5833333 C9.75003686,19.5882688 11.6168794,18.8726691 13.0522917,17.5760417 C16.0171492,20.2556967 20.5292675,20.2556967 23.494125,17.5760417 C26.4604562,20.2616016 30.9794188,20.2616016 33.94575,17.5760417 C36.2421905,19.6477597 39.5441143,20.1708521 42.3684437,18.9103691 C45.1927731,17.649886 47.0084685,14.8428276 47.0000295,11.75 C47.0000295,11.3946378 46.9030823,11.0460037 46.7199583,10.7414583 Z",
   id: "Path",
-  opacity: "0.6"
+  opacity: "0.598981585"
 }), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("path", {
   "class": "color-background",
-  d: "M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z",
-  id: "Shape"
+  d: "M39.198,22.4912623 C37.3776246,22.4928106 35.5817531,22.0149171 33.951625,21.0951667 L33.92225,21.1107282 C31.1430221,22.6838032 27.9255001,22.9318916 24.9844167,21.7998837 C24.4750389,21.605469 23.9777983,21.3722567 23.4960833,21.1018359 L23.4745417,21.1129513 C20.6961809,22.6871153 17.4786145,22.9344611 14.5386667,21.7998837 C14.029926,21.6054643 13.533337,21.3722507 13.0522917,21.1018359 C11.4250962,22.0190609 9.63246555,22.4947009 7.81570833,22.4912623 C7.16510551,22.4842162 6.51607673,22.4173045 5.875,22.2911849 L5.875,44.7220845 C5.875,45.9498589 6.7517757,46.9451667 7.83333333,46.9451667 L19.5833333,46.9451667 L19.5833333,33.6066734 L27.4166667,33.6066734 L27.4166667,46.9451667 L39.1666667,46.9451667 C40.2482243,46.9451667 41.125,45.9498589 41.125,44.7220845 L41.125,22.2822926 C40.4887822,22.4116582 39.8442868,22.4815492 39.198,22.4912623 Z",
+  id: "Path"
 })])])])])])], -1
 /* HOISTED */
 );
@@ -17805,7 +17811,7 @@ __webpack_require__.r(__webpack_exports__);
 var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
 exports = ___CSS_LOADER_API_IMPORT___(false);
 // Module
-exports.push([module.id, "\n.router-link-active[data-v-2053f31b]{\n    color: #252f40;\n    background-color: #fff;\n    font-weight: 600;\n    box-shadow: 0 20px 27px 0 rgb(0 0 0 / 5%);\n    border-radius: 0.5rem;\n}\n.router-link-active .icon[data-v-2053f31b] {\n    background-image: linear-gradient(\n        310deg\n        , #cb0c9f 0%, #cb0c9f 100%);\n    fill: #fff;\n}\n.navbar-vertical .navbar-nav > .nav-item .nav-link.router-link-active .icon svg .color-background[data-v-2053f31b], .navbar-vertical .navbar-nav > .nav-item .nav-link.router-link-active .icon svg .color-foreground[data-v-2053f31b] {\n    fill: #fff;\n}\n\n\n\n", ""]);
+exports.push([module.id, "\n.router-link-active[data-v-2053f31b]{\n    color: #252f40;\n    background-color: #fff;\n    font-weight: 600;\n    box-shadow: 0 20px 27px 0 rgb(0 0 0 / 5%);\n    border-radius: 0.5rem;\n}\n.navbar-vertical .navbar-nav>.nav-item .nav-link.router-link-active .icon[data-v-2053f31b] {\n    background-image: linear-gradient(\n        310deg\n        , #cb0c9f 0%, #cb0c9f 100%);\n    fill: #fff;\n}\n.navbar-vertical .navbar-nav > .nav-item .nav-link.router-link-active .icon svg .color-background[data-v-2053f31b], .navbar-vertical .navbar-nav > .nav-item .nav-link.router-link-active .icon svg .color-foreground[data-v-2053f31b] {\n    fill: #fff;\n}\n\n\n\n", ""]);
 // Exports
 module.exports = exports;
 
--- resources/js/components/Layouts/Navbar.vue
+++ resources/js/components/Layouts/Navbar.vue
@@ -2,10 +2,6 @@
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 border-radius-xl position-sticky blur shadow-blur mt-4 left-auto top-1 z-index-sticky" id="navbarBlur" navbar-scroll="true">
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                    <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Tables</li>
-                </ol>
                 <h6 class="font-weight-bolder mb-0">Tables</h6>
             </nav>
             <div class="navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar" style="flex-flow: row-reverse;">
